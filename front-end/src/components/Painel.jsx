6d27b08e58b1aa4bdfd394ce47e97721718e1813
Rafhael Oliveira <74227915+Underewarrr@users.noreply.github.com>
2022-07-30 01:06:11 -0300

    Dev: Desenvolve logica para pegar usario e adiciona na lista de rede sociais

--- front-end/src/components/Painel.jsx
+++ front-end/src/components/Painel.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Footer from './Footer'
 import Header from './Header'
-import RegisterSocialMedia from './RegisterSocialMedia'
+import RegistredSocialMedia from './RegistredSocialMedia'
 
 const Painel = () => {
   return (
     <div>
         <Header />
+        <RegistredSocialMedia />
         <Footer />
     </div>
   )
