34dca87e5be9f9019986190feaaf625e3d172654
Rafhael Oliveira <74227915+Underewarrr@users.noreply.github.com>
2022-08-02 08:03:39 -0300

    Fix: Registrando usuario na database removendo o username

--- back-end/index.js
+++ back-end/index.js
@@ -29,13 +29,15 @@ app.get('/', (req, res) => {
 
 app.post('/auth/register', async (req, res) => {
     const { username, email, password, confirmPassword } = req.body;
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
     db.query("SELECT * FROM users WHERE email = ?", [email], (err, result) => {
         if (password !== confirmPassword) {
             return res.status(400).json({
                 message: 'Passwords do not match'
             });
         }
-        if (!username || !email || !password) {
+        if (!email || !password) {
             return res.status(400).json({
                 message: 'Please fill in all fields'
             });
@@ -50,18 +52,15 @@ app.post('/auth/register', async (req, res) => {
                 message: 'Please enter a valid email address'
             });
         }
-        if (username.length < 3) {
-            return res.status(400).json({
-                message: 'Username must be at least 3 characters long'
-            });
-        }
         if (result.length > 0) {
             return res.status(400).json({
                 message: 'Email already exists'
             });
         }
         else {
-            db.query("INSERT INTO users (email, password) VALUES (?, ?)", [email, password], (err, result) => {
+            // Hash password
+            // Insert user into database
+            db.query("INSERT INTO users (email, password) VALUES (?, ?)", [email, hashedPassword], (err, result) => {
                 if (err) {
                     return res.status(400).json({
                         message: 'Something went wrong'
