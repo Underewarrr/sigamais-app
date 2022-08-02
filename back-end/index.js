aee6d01d1e8bfb5a2215900d9d325e9589cc2e1d
Rafhael Oliveira <74227915+Underewarrr@users.noreply.github.com>
2022-08-02 01:38:36 -0300

    Dev: Começa do inicio back-end

--- back-end/index.js
+++ back-end/index.js
@@ -1,143 +1,21 @@
+require('dotenv').config();
 const express = require('express');
-const app = express();
 const mysql = require('mysql');
 const cors = require('cors');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-const db = mysql.createPool({
-    host: '127.0.0.1',
-    user: 'root',
-    password: '',
-    database: 'sigamais'
-});
-
-app.get('/painel/get/users_instagram', (req, res) => {
-    db.query(`SELECT * FROM users_instagram`, (err, results) => {
-        if (err) {
-            res.json({
-                status: 'error',
-                message: err
-            })
-        } else {
-            res.json({
-                status: 'success',
-                data: {
-                    id: results[0].id,
-                    username: results[0].username,
-                    password: results[0].password,
-                    email: results[0].email,
-
-                }
-            })
-
-
-
-        }
-    })
-})
-
-app.post('/painel/add/users_instagram', (req, res) => {
-    const { email, username, codsecurity} = req.body;
-    db.query("SELECT * FROM users_instagram WHERE email = ?", [email], (err, result) => {
-        if (result.length > 0) {
-            res.json({
-                status: 'error',
-                message: 'Email já cadastrado'
-            });
-        } else {
-            db.query("INSERT INTO users_instagram (email, username, codsecurity) VALUES (?, ?, ?)", [email, username, codsecurity], (err, result) => {
-                if (err) {
-                    res.json({
-                        status: 'error',
-                        message: 'Erro ao cadastrar'
-                    });
-                } else {
-                    res.json({
-                        status: 'success',
-                        message: 'Cadastrado com sucesso',
-                        result: {
-                            id: result.insertId,
-                            email: email,
-                            username: username,
-                            codsecurity: codsecurity,
-
-                        }
-                    });
-                }
-            });
-        }
+// Public routes
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: 'Welcome'
     });
 });
-    
-app.post("/register", (req, res) => {
-    const { email, password } = req.body;
-  
-    db.query("SELECT * FROM users WHERE email = ?", [email], (err, result) => {
-       
-            if (result.length > 0) {
-                res.json({
-                    status: 'error',
-                    message: 'Email já cadastrado'
-                });
-
-            } else {
-                db.query("INSERT INTO users (email, password) VALUES (?, ?)", [email, password], (err, result) => {
-                    if (err) {
-                        res.json({
-                            status: 'error',
-                            message: 'Erro ao cadastrar'
-                        });
-                    } else {
-                        res.json({
-                            status: 'success',
-                            message: 'Cadastrado com sucesso'
-                        });
-                    }
-                });
-        }
-        }
-    );
-  });
-
-  app.post("/login", (req, res) => {
-    const { email, password } = req.body;
-    db.query("SELECT * FROM users WHERE email = ?", [email], (err, result) => {
-        if (err) {
-            res.json({
-                status: 'error',
-                message: 'Erro ao logar'
-            });
-            
-        } else {
-            if (result.length > 0) {
-                if (result[0].password === password) {
-                    res.json({
-                        status: 'success',
-                        message: 'Login realizado com sucesso'
-                    });
-
-                } else {
-                    res.json({
-                        status: 'error',
-                        message: 'Senha incorreta'
-                    });
-
-                }
-            } else {
-                res.json({
-                    status: 'error',
-                    message: 'Email não cadastrado'
-                });
-            }
-        }
-        }
-    );
-  });
-
-
 app.listen(3001,  () => {
     console.log('Server is running on port 3001');
 });
