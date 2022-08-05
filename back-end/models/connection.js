2bda4b1324b724dae80ced218eed7d25f57e6f32
Rafhael Oliveira <underewar@gmail.com>
2022-08-05 19:35:51 -0300

    Fix: Arruma rescue na pasta errada e nome da db

--- back-end/models/connection.js
+++ back-end/models/connection.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2/promise');
 const connection = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: 'password',
-  database: 'model_database'
+  password: '',
+  database: 'appweb'
 });
 
 module.exports = connection;
